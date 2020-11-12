6b4f6546734f67f35f28f3e7306b0eba547ca05b
Mike North <michael.l.north@gmail.com>
2020-11-12 15:13:31 -0800

    add non-namespace example

--- dist/index.js
+++ dist/index.js
@@ -1,7 +1,12 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
-const express = require("express");
-const app = express();
-app.listen(3000, function () {
+const express1 = require("express");
+const express2 = require("express");
+const app1 = express1();
+app1.listen(3000, function () {
     console.log("listening on :3000");
+});
+const app2 = express2();
+app2.listen(3001, function () {
+    console.log("listening on :3001");
 });
--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,12 @@
-import * as express from "express";
+import * as express1 from "express";
+import express2 = require("express");
 
-const app = express();
-app.listen(3000, function () {
+const app1 = express1();
+app1.listen(3000, function () {
   console.log("listening on :3000");
+});
+
+const app2 = express2();
+app2.listen(3001, function () {
+  console.log("listening on :3001");
 });
